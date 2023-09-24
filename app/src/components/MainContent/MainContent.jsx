95a2d77ad21bdbb0f8cb288a96f95b22c8bbbe6b
percius47 <33738242+percius47@users.noreply.github.com>
2023-09-24 21:56:40 +0530

    feat: sidebar+main using module css

---
+++ app/src/components/MainContent/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import MainContentCSS from "./MainContent.module.css"
+function MainContent() {
+  return (
+  <div className="MainContent">
+    main
+  </div>
+  )
+}
+
+export default MainContent
---
+++ app/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SidebarCSS from "./Sidebar.module.css"
+function Sidebar() {
+  return (
+    <div className={SidebarCSS.SidebarParent}>
+      Sidebar
+    </div>
+  )
+}
+
+export default Sidebar
