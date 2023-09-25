99cfc584b7113b18b8a5105d052f2c8ef334954c
thinkmetalWeb <122011218+thinkmetalWeb@users.noreply.github.com>
2023-09-25 16:08:48 +0530

    feat: slicer mounted on slicerScreen

--- app/src/pages/SlicerScreen/SlicerScreen.jsx
+++ app/src/pages/SlicerScreen/SlicerScreen.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import styles from "./SlicerScreen.module.css"
 function SlicerScreen() {
   return (
-    <div>SlicerScreen</div>
+    <div className={styles.parent}>
+       <iframe src="http://13.127.238.43:8100/kiri/"
+        title='slicer' frameborder="0" 
+        className={styles.SlicerFrame}></iframe>
+    </div>
   )
 }
 
