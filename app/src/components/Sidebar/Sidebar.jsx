5906b9509d09fe00716bbde9956230559e03a23a
thinkmetalWeb <122011218+thinkmetalWeb@users.noreply.github.com>
2023-09-25 15:00:04 +0530

    feat: user account info and logout btn

--- app/src/components/Sidebar/Sidebar.jsx
+++ app/src/components/Sidebar/Sidebar.jsx
@@ -1,36 +1,51 @@
-import React from 'react'
-import SidebarCSS from "./Sidebar.module.css"
-import { NavLink } from 'react-router-dom'
-import UserIcon from "../../assets/user.png"
+import React from "react";
+import SidebarCSS from "./Sidebar.module.css";
+import { NavLink } from "react-router-dom";
+import UserIcon from "../../assets/user.png";
 function Sidebar() {
   return (
     <div className={SidebarCSS.SidebarParent}>
+      <div>
       <div className={SidebarCSS.appname}>
         <h3>thinkware</h3>
       </div>
 
       <div className={SidebarCSS.pageList}>
-       
-        <NavLink  to ="/" 
-        className={({ isActive }) =>isActive?SidebarCSS.activeList:SidebarCSS.pageItem}>
-               Dashboard
-            </NavLink>
-            <NavLink to="/job" className={({ isActive }) =>isActive?SidebarCSS.activeList:SidebarCSS.pageItem}>
-                Job
-            </NavLink>
-            <NavLink to="/slice"  className={({ isActive }) =>isActive?SidebarCSS.activeList:SidebarCSS.pageItem}>
-               Slice
-            </NavLink>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? SidebarCSS.activeList : SidebarCSS.pageItem
+          }
+        >
+          Dashboard
+        </NavLink>
+        <NavLink
+          to="/job"
+          className={({ isActive }) =>
+            isActive ? SidebarCSS.activeList : SidebarCSS.pageItem
+          }
+        >
+          Job
+        </NavLink>
+        <NavLink
+          to="/slice"
+          className={({ isActive }) =>
+            isActive ? SidebarCSS.activeList : SidebarCSS.pageItem
+          }
+        >
+          Slice
+        </NavLink>
       </div>
+            </div>
       <div className={SidebarCSS.UserAccount}>
-          <div className={SidebarCSS.UserInfo}>
-            {/* <img src={UserIcon} alt="" /> */}
-            <h5>John Doe</h5>
-          </div>
-
+        <div className={SidebarCSS.UserInfo}>
+          <img src={UserIcon} alt="" />
+          <h5>John Doe</h5>
+        </div>
+        <button className={SidebarCSS.Logoutbtn}>Logout</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
