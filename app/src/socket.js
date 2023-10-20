2a9c0de5328646bbf0e3775bf02d75c08dece192
thinkmetal-shbhm <shubham@thinkmetal.in>
2023-10-20 11:54:24 +0530

    fix:socket url for raspberrypi

--- app/src/socket.js
+++ app/src/socket.js
@@ -2,6 +2,8 @@ import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
-  process.env.NODE_ENV === "production" ? undefined : "http://localhost:4000";
+  process.env.NODE_ENV === "production"
+    ? undefined
+    : "http://raspberrypi.local:4000";
 
 export const socket = io(URL);
