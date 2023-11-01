01a0e45faa96388d77a18d1a874d0ba6108b9fbf
thinkmetal-shbhm <shubham@thinkmetal.in>
2023-11-01 14:23:17 +0530

    fix:socket setIO to setSocket

--- app/src/socket.js
+++ app/src/socket.js
@@ -5,7 +5,7 @@ let socket;
 const URL =
   process.env.NODE_ENV === "production" ? undefined : "http://localhost:4000";
 
-setIO(URL);
+setSocket(URL);
 
 export function getSocket() {
   return socket;
