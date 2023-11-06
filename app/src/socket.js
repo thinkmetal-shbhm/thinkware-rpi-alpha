507088331e5e44d36ce142712310eb2209dea956
thinkmetal-shbhm <shubham@thinkmetal.in>
2023-11-06 12:11:19 +0530

    fix:socket error

--- app/src/socket.js
+++ app/src/socket.js
@@ -2,12 +2,12 @@ import { io } from "socket.io-client";
 
 let socket;
 
-const URL =
-  process.env.VITE_NODE_ENV === "production"
-    ? undefined
-    : "http://localhost:4000";
+// const URL =
+//   process.env.VITE_NODE_ENV === "production"
+//     ? undefined
+//     : "http://localhost:4000";
 
-setSocket(URL);
+// setSocket(URL);
 
 export function getSocket() {
   return socket;
