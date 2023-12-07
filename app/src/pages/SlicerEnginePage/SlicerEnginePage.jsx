6b6067893c97767af5276e55ffcf45263d7a2d72
thinkmetal-shbhm <shubham@thinkware.in>
2023-12-07 21:40:53 +0530

    feat:position state

--- app/src/pages/SlicerEnginePage/SlicerEnginePage.jsx
+++ app/src/pages/SlicerEnginePage/SlicerEnginePage.jsx
@@ -1,12 +1,13 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import SlicerEngine from "../../components/SlicerEngine";
 
 function SlicerEnginePage() {
   const fileRef = useRef(null);
+  const [pos, setPos] = useState({ x: 0, y: 0 });
 
   return (
     <React.Fragment>
-      <SlicerEngine fileRef={fileRef} />
+      <SlicerEngine fileRef={fileRef} pos={pos} setPos={setPos} />
     </React.Fragment>
   );
 }
